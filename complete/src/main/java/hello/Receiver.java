4587a5d38857b401768e5e183f5473dc83cb39c2
liuhaoyong <liuhaoyong@tuhu.ad>
2017-10-20 13:55:12 +0800

    代码修改

--- complete/src/main/java/hello/Receiver.java
+++ complete/src/main/java/hello/Receiver.java
@@ -10,7 +10,7 @@ public class Receiver {
     private CountDownLatch latch = new CountDownLatch(1);
 
     public void receiveMessage(byte[] message) {
-        System.out.println("Received <" + message + ">");
+        System.out.println("Received <" + new String(message) + ">");
         latch.countDown();
     }
 
